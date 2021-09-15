e994de5abf56f4f3afef289519dcce7de41a7a40
Dennis-A Tepel <>
2021-09-15 14:13:12 +0200

    Fixed Assert Statement

--- src/test/java/org/xpdojo/bank/AccountTest.java
+++ src/test/java/org/xpdojo/bank/AccountTest.java
@@ -3,21 +3,25 @@ package org.xpdojo.bank;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
+
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
 
 public class AccountTest {
 
     @Test
     public void startingBalanceIsZero() {
         Account account = new Account();
-        assertThat(account.balance == 0);
+        assertThat(account.balance,is(0));
     }
+
 
     @Test
     public void depositAnAmountToIncreaseTheBalance() {
         Account account = new Account();
         account.deposit(100);
-        assertThat(account.balance == 100);
+        assertThat(account.balance, is(100));
     }
 
     @Test
@@ -25,6 +29,6 @@ public class AccountTest {
         Account account = new Account();
         account.deposit(100);
         account.deposit(100);
-        assertThat(account.balance == 200);
+        assertThat(account.balance,is(200));
     }
 }
